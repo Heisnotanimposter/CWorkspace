843f4cad1be2cc54c410ccb2a406cf07dadc1701
SeungwonLee <97718938+Heisnotanimposter@users.noreply.github.com>
2024-04-09 17:54:45 +0900

    1
    1

--- parsingtable/testcode_term2/NT.h
+++
@@ -1,4 +0,0 @@
-#define NT
-
-char NT[] = {' ', 'E', 'E', 'T', 'T', 'F', 'F'};
-int NT_len[] = {0, 1, 1, 1, 1, 1, 1};
--- parsingtable/testcode_term2/RHS.h
+++
@@ -1,4 +0,0 @@
-#define RHS
-
-char *RHS[] = {' ', 'E+T', 'T', 'T*F' , 'F' ,'(E)' ,'d'};
-int RHS_len[] = {0, 3, 1, 3, 1, 3, 1};
